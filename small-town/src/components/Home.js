2f4da2170e559ca152de9e8157b058db7e933b33
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-06-17 17:12:22 +0300

    Home component and style

---
+++ small-town/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Small Town</h1>
+			<img className="map" alt="MAP" src="./small-town-map.png" />
+			<p>Small Town is a weird place where people are trying to survive.</p>
+			<p>Some may leave, some may stay, but in the end the Small Town have the same people.</p>
+		</div>
+	);
+};
+
+export default Home;
