757acc74c616c4466a1310a413dc706c1cdec1b9
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-06-17 17:32:54 +0300

    adding router

--- small-town/src/App.js
+++ small-town/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
 
 import Home from './components/Home';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav>
+				<NavLink to="/">Home</NavLink>
+			</nav>
+			<div>
+				<Route path='/' component={Home} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
